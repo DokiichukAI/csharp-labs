8eb0bde70c66f0a046e9a8ecd8a8b19bf448bf90
ZloyHohol <zloyhohol16@gmail.com>
2022-10-15 16:12:48 +0300

    Firts Lab

---
+++ WeaponInfo.io/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace WeaponInfo.io {
+    class program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Title = "WeaponInfo.io (Докийчук А.И) ";
+            Console.WriteLine("Робота сделаная для поиска воружения стран по разным типам ");
+            Console.ReadKey(true);
+
+            static void OutData()
+            {
+
+
+
+            }
+
+
+        }
+    }    
+}
